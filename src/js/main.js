37829f520bafdd253a20eb92222f0cb70b3f453a
JDC <jdcdevelopmenrcompany@gmail.com>
2017-11-12 15:20:20 +0300

    Develop cards

--- src/js/main.js
+++ src/js/main.js
@@ -4,3 +4,19 @@ $(document).ready(function(){
 		$(this).siblings('#product ul li').slideToggle(0);
 	});
 });
+
+/*------SLIDER------*/
+autoSlider();
+var left = 0;
+function autoSlider(){
+    var timer = setTimeout(function(){
+      var sliderWidth = document.getElementById('my-slider');
+        left = left - 620;
+        if (left < -1794){
+            left = 0;
+            clearTimeout(timer);
+        }
+        sliderWidth.style.left = left + 'px';
+        autoSlider();  
+    },3000);
+}
