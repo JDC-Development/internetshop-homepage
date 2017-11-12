e84362d4f0b0fe1bbef8db279012f7c5a1de1750
JDC <jdcdevelopmenrcompany@gmail.com>
2017-11-12 23:33:22 +0300

    Final

--- src/js/main.js
+++ src/js/main.js
@@ -29,3 +29,29 @@ $(document).ready(function(){
     $('.head-basket').text('There are currently ' +count+ ' items in your basket!');  
     });
 });
+
+$(document).ready(function(){
+    $('#menu-toogle').on('click',function(){
+        $(this).siblings('aside').slideToggle(0);
+    });
+});
+
+$(document).ready(function(){
+    $('#top-menu').on('click', '.menu-adaptiv', function() {
+		$(this).siblings('#top-menu ul li').slideToggle(0);
+	});
+});
+
+var h_hght = 115; // высота шапки
+var h_mrg = 0;     // отступ когда шапка уже не видна
+$(function(){
+    $(window).scroll(function(){
+       var top = $(this).scrollTop();
+       var elem = $('#menu-top');
+       if (top+h_mrg < h_hght) {
+        elem.css('top', (h_hght-top));
+       } else {
+        elem.css('top', h_mrg);
+       }
+     });
+   });
